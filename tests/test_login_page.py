4fc5bd4852b57f4a257c6900040ab39c5331c767
Joan Esquivel <48256183+JoanEsquivel@users.noreply.github.com>
2023-10-04 21:20:25 -0600

    feat: valid login basic scenario

--- tests/test_login_page.py
+++ tests/test_login_page.py
@@ -1,7 +1,26 @@
 import time
+from selenium.webdriver.common.by import By
 
 
 class TestLoginpage:
     def test_valid_login(self, driver):
-        driver.get("https://www.google.com")
-        time.sleep(10)
+        driver.get("https://www.saucedemo.com/")
+        time.sleep(2)
+
+        # Type username
+        username_input = driver.find_element(By.ID, "user-name")
+        username_input.send_keys("standard_user")
+
+        # Type password
+        password_input = driver.find_element(By.ID, "password")
+        password_input.send_keys("secret_sauce")
+
+        # Click on the login button
+        login_btn = driver.find_element(By.ID, "login-button")
+        login_btn.click()
+
+        # URL Validation
+        actual_url = driver.current_url
+        assert actual_url == "https://www.saucedemo.com/inventory.html"
+
+        time.sleep(2)
