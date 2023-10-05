9586aa59fbd9f492160ffad478946f39eebfe1e6
Joan Esquivel <48256183+JoanEsquivel@users.noreply.github.com>
2023-10-05 02:18:29 -0600

    fix: tests should pass

--- tests/test_login_page.py
+++ tests/test_login_page.py
@@ -22,7 +22,7 @@ class TestLoginpage:
 
         # URL Validation
         actual_url = driver.current_url
-        assert actual_url == "https://www.saucedemo.com/inventory.htmls"
+        assert actual_url == "https://www.saucedemo.com/inventory.html"
 
         # time.sleep(2)
 
